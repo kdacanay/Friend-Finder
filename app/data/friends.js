e3cbcc0a5dca25ca7ae3704c503307294b02605f
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-05-05 23:57:48 -0400

    added friendsArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,60 @@
 //----app's data held in friends.js as an array of objects
+
+var friendsArray = [{
+    name: 'Ken',
+    photo: '',
+    scores: [
+        '4', '4', '5', '3', '2', '2', '5', '5', '3', '3'
+    ]
+}, {
+    name: 'Glenn',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Tommy',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Hoff',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Jimmy',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Tony',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Steve',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Mike',
+    photo: '',
+    scores: [
+
+    ]
+}, {
+    name: 'Eric',
+    photo: '',
+    scores: [
+
+    ]
+}]
+
+
+module.exports = friendsArray;
